13f70ef042e1ebde70967c49db6311ec8ec3eba7
michael bahl <michael.bahl1984@gmx.de>
2017-10-29 13:37:22 +0100

    Add notification sound and make notification appear at the top

--- android/Notify/app/src/main/java/com/kevinbedi/notify/NotifyFirebaseMessagingService.java
+++ android/Notify/app/src/main/java/com/kevinbedi/notify/NotifyFirebaseMessagingService.java
@@ -1,14 +1,21 @@
 package com.kevinbedi.notify;
 
 import android.app.NotificationManager;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.support.v4.app.NotificationCompat;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
+import static android.app.Notification.PRIORITY_MAX;
+
 public class NotifyFirebaseMessagingService extends FirebaseMessagingService {
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
+
+        Uri alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
         NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
         manager.notify(
                 remoteMessage.getMessageId(),
@@ -18,6 +25,8 @@ public class NotifyFirebaseMessagingService extends FirebaseMessagingService {
                         .setContentTitle(remoteMessage.getNotification().getTitle())
                         .setContentText(remoteMessage.getNotification().getBody())
                         .setVibrate(new long[] { 150, 300, 150, 600})
+                        .setSound(alarmSound)
+                        .setPriority(PRIORITY_MAX)
                         .setAutoCancel(true)
                         .build());
         super.onMessageReceived(remoteMessage);
