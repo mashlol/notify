073f6d4f7bf6292c3d2f2d3700f0d770de437002
Michał Leśniewski <mlesniew@gmail.com>
2018-10-09 19:22:03 +0200

    Improve notify.sh
    Changes:
      * script now uses getopt for option parsing
      * added error checking
      * escaping of spaces and other special chars is now done by curl
      * added help message
      * general clean up and refactoring

--- sh/notify.sh
+++ sh/notify.sh
@@ -1,34 +1,80 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-while [[ $# > 1 ]]
-do
-  key="$1"
+TOKENFILE=~/.notifyreg
 
-  case $key in
-      -r|--register)
-      KEY="$2"
-      shift
-      ;;
-      -t|--text)
-      TEXT=$(sed 's/ /%20/g' <<< "$2")
-      shift
-      ;;
-      *)
-      ;;
-  esac
+function usage()
+{
+    echo "Send notification to an Android device."
+    echo
+    echo "usage:"
+    echo "  notify.sh --register <token>"
+    echo "  notify.sh --text <text>"
+    echo
+    echo "options:"
+    echo "  --register <token>, -r <token>    Store given token in token file"
+    echo "  --text <text>, -t <text>          Text for the notification"
+}
 
-  shift
+TEMP=$(getopt -o hr:t: --long help,register:,text: -n 'notify.sh' -- "$@")
+eval set -- "$TEMP"
+
+TEXT=""
+
+while true; do
+    case "$1" in
+        -r|--register)
+              echo "Storing new token in $TOKENFILE."
+              echo "$2" > "$TOKENFILE"
+              exit
+            ;;
+        -t|--text)
+            TEXT=$2
+            shift 2
+            ;;
+        -h|--help)
+            usage
+            exit
+            ;;
+        --)
+            shift
+            break
+            ;;
+        *)
+            echo "Internal error."
+            exit 1 ;;
+    esac
 done
 
-if [ -n "${KEY}" ]
+if [ "$#" -ne 0 ]
 then
-  # Save token to ~/.notifyreg
-  echo "${KEY}" > ~/.notifyreg
-else
-  KEY=`cat ~/.notifyreg`
-  curl -g\
-  "https://us-central1-notify-b7652.cloudfunctions.net/sendNotification?to=${KEY}&text=${TEXT}" \
-  > /dev/null
+    echo "Unexpected positional arguments."
+    exit 1
+fi
 
-  echo "[notify] Successfully sent notification."
+if [ ! -f "$TOKENFILE" ]
+then
+    echo "Token file missing at $TOKENFILE"
+    echo "Use --register to specify token."
+    exit 2
+fi
+
+TOKEN=$(cat "$TOKENFILE")
+
+if [ -z "$TOKEN" ]
+then
+    echo "Token file empty."
+    echo "Use --register to specify new token."
+    exit 2
+fi
+
+if curl -g -s -G \
+    'https://us-central1-notify-b7652.cloudfunctions.net/sendNotification' \
+    --data-urlencode "to=$TOKEN" \
+    --data-urlencode "text=$TEXT" \
+    >/dev/null 2>&1
+then
+    echo "[notify] Successfully sent notification."
+else
+    echo "[notify] Sending notification failed."
+    exit 3
 fi
